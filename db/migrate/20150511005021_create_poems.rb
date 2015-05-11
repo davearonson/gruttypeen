2511164a017676ba229eb97f1778433c00236938
Dave Aronson <github.2.trex@codosaur.us>
2015-05-10 21:39:04 -0400

    add author and year to poem

--- db/migrate/20150511005021_create_poems.rb
+++ db/migrate/20150511005021_create_poems.rb
@@ -1,9 +1,11 @@
 class CreatePoems < ActiveRecord::Migration
   def change
     create_table :poems do |t|
-      t.string :title,   null: false
-      t.text   :text,    null: false
-      t.string :prompts, null: false, default: [], array: true
+      t.string  :title,   null: false
+      t.string  :author,  null: false, default: "Unknown"
+      t.integer :year,    null: true
+      t.text    :text,    null: false
+      t.string  :prompts, null: false, default: [], array: true
       t.timestamps
     end
   end
