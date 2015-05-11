b44bcf294c175eda788a792a01d40b495dc4a9a4
Dave Aronson <github.2.trex@codosaur.us>
2015-05-11 19:57:53 -0400

    remove initializer no longer needed w/ new syntax

--- spec/models/poem_spec.rb
+++ spec/models/poem_spec.rb
@@ -6,8 +6,7 @@ describe Poem do
 
     it "substitutes responses " do
       poem = Poem.new(title: "doesn't matter for this test",
-                      text: "Shall I @{compare: irrelevant} thee to a summer's @{day: irrelevant}? @{dayglo} @{smurf} @{compare}",
-                      prompts: %w(irrelevant for this test))
+                      text: "Shall I @{compare: irrelevant} thee to a summer's @{day: irrelevant}? @{dayglo} @{smurf} @{compare}")
       responses = { "compare" => "unlock", "day" => "brick", "smurf" => "beyond" }
       expect(poem.fill_with responses).
         to eq "Shall I unlock thee to a summer's brick? @{dayglo} beyond unlock"
