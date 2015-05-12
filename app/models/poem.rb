4f56ccea30c7a1c8ca433a48ecdc2718d7798a3d
Dave Aronson <github.2.trex@codosaur.us>
2015-05-12 07:04:50 -0400

    add extraction of prompts

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,10 +1,28 @@
 class Poem < ActiveRecord::Base
+
   def fill_with(responses)
     result = text.dup
     responses.each do |key, val|
-      result.gsub!(/@{#{key}: [^}]+}/, val) # 1st use, eg @{tree: noun, one syllable}
+      result.gsub!(descriptor_regex(key), val) # 1st use, eg @{tree: noun, one syllable}
       result.gsub!(/@{#{key}}/, val) # later uses, like @{tree}
     end
     result
   end
+
+  def prompts
+    results = {}
+    tmp_text = text.dup
+    while (matches = tmp_text.match descriptor_regex("\\w*"))
+      results[matches[1]] = matches[2]
+      tmp_text.sub!(matches[0], "")
+    end
+    results
+  end
+
+  private
+
+  def descriptor_regex(key)
+    /@{(#{key}): ([^}]+)}/
+  end
+
 end
--- spec/models/poem_spec.rb
+++ spec/models/poem_spec.rb
@@ -2,16 +2,18 @@ require "rails_helper"
 
 describe Poem do
 
-  describe "#fill_with" do
+  let(:poem) { Poem.new(title: "doesn't matter for this test",
+                        text: "Shall I @{compare: irrelevant} thee to a summer's @{day: doesn't matter}? @{dayglo} @{smurf} @{compare}") }
 
-    it "substitutes responses " do
-      poem = Poem.new(title: "doesn't matter for this test",
-                      text: "Shall I @{compare: irrelevant} thee to a summer's @{day: irrelevant}? @{dayglo} @{smurf} @{compare}")
-      responses = { "compare" => "unlock", "day" => "brick", "smurf" => "beyond" }
-      expect(poem.fill_with responses).
-        to eq "Shall I unlock thee to a summer's brick? @{dayglo} beyond unlock"
-    end
+  it "#fill_with substitutes responses " do
+    responses = { "compare" => "unlock", "day" => "brick", "smurf" => "beyond" }
+    expect(poem.fill_with responses).
+      to eq "Shall I unlock thee to a summer's brick? @{dayglo} beyond unlock"
+  end
 
+  it "#prompts finds the prompts" do
+    expect(poem.prompts).to eq({ "compare" => "irrelevant",
+                                 "day" => "doesn't matter" })
   end
 
 end
