77a7175ca5fad0e3481c0265931d2fd28efc518c
Dave Aronson <github.2.trex@codosaur.us>
2015-05-26 20:40:40 -0400

    deal with leftovers and different case

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -2,9 +2,24 @@ class Poem < ActiveRecord::Base
 
   def fill_with(responses)
     result = text.dup
-    responses.each_with_index do |(old_word, substitute), index|
-      result.gsub!(prompt_regex(old_word), substitute)  # prompt
-      result.gsub!(/@{#{old_word}}/, substitute)  # back-refs
+    responses.each do |old_word, substitute|
+      new_word = substitute.dup  # in case we need to capitalize it
+      new_word.capitalize! if /[A-Z]/.match(old_word[0])
+      result.gsub!(prompt_regex(old_word), new_word)  # prompt
+      result.gsub!(/@{#{old_word}}/, new_word)  # back-refs
+    end
+    # if any left, try with case swapped, else just mark bad
+    while (matches = /@{(?<old_word>\w+)(: [^}]+)?}/.match(result))
+      old_word = matches[:old_word]
+      new_word = old_word.capitalize
+      substitute = responses[new_word]
+      if substitute.present?
+        substitute.capitalize! if /[A-Z]/.match(old_word[0])
+        result.gsub!(matches[0], substitute)
+      else
+        # swapped version isn't there either so just mark it bad
+        result.gsub!(matches[0], matches[0].gsub!("@{", "@{ERROR - "))
+      end
     end
     result
   end
@@ -35,6 +50,12 @@ class Poem < ActiveRecord::Base
       number = words.find_index matches[1]
       prompt.gsub! matches[0], "##{number + 1}"
     end
+  end
+
+  def sub_in(str, old_word, substitute)
+    substitute ||= "@[!#{old_word}!]"
+    str.gsub!(prompt_regex(old_word), substitute)  # prompt
+    str.gsub!(/@{#{old_word}}/, substitute)  # back-refs
   end
 
 end
--- spec/models/poem_spec.rb
+++ spec/models/poem_spec.rb
@@ -4,24 +4,25 @@ describe Poem do
 
   # note especially a prompt at the very beginning, a back-ref at the end,
   # some back-refs inside prompts, and an *unsatisfied* backref in the middle
-  let(:poem) { Poem.new(title: "Trees",
-                        text: "@{Trees: noun, plural, one syllable}\n"\
-                        "@{I: pronoun} think that I shall never"\
-                        " @{see: verb, one syllable, rhyming with the singular of @Trees@}\n"\
-                        "A @{poem: noun, two syllables, accent on the first} @{surprise}"\
-                        " @{lovely: adjective, two syllables, accent on the first} as a"\
-                        " @{tree: singular of @Trees@} @{I}") }
+  let(:poem) { Poem.new(
+    title: "Trees",
+    text: "@{Trees: noun, plural, one syllable}\n"\
+    "@{I: pronoun} think that I shall never"\
+    " @{see: verb, one syllable, rhyming with the singular of @Trees@}\n"\
+    "A @{poem: noun, two syllables, accent on the first} @{surprise}"\
+    " @{lovely: adjective, two syllables, accent on the first} as a"\
+    " @{tree: singular of @Trees@} @{boo: blah blah blah} @{i}") }
 
-  it "#fill_with substitutes responses " do
+  it "#fill_with substitutes responses, with proper case" do
     responses = { "Trees" => "boats",
-                  "I" => "ye",
+                  "I" => "they",
                   "see" => "float",
                   "poem" => "boar's head",
                   "lovely" => "tired",
                   "tree" => "boat" }
     expect(poem.fill_with responses).
-      to eq("boats\nye think that I shall never float\n"\
-            "A boar's head @{surprise} tired as a boat ye")
+      to eq("Boats\nThey think that I shall never float\n"\
+            "A boar's head @{ERROR - surprise} tired as a boat @{ERROR - boo: blah blah blah} they")
   end
 
   it "#prompts finds the prompts and translates number back-refs" do
@@ -31,7 +32,8 @@ describe Poem do
       "see" => "verb, one syllable, rhyming with the singular of #1",
       "poem" => "noun, two syllables, accent on the first",
       "lovely" => "adjective, two syllables, accent on the first",
-      "tree" => "singular of #1"
+      "tree" => "singular of #1",
+      "boo"=>"blah blah blah"
     })
   end
 
